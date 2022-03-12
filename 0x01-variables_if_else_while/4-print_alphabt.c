1cddcd61cdcb5149eadc825ad3872bbf4cf5a6ed
Giddyyy <gideonoluniyi@gmail.com>
2022-03-11 16:03:51 -0800

    C var

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,15 @@
 int main(void)
 
 {
-int ch;
-for (ch = 'a'; ch <= 'z'; ch++)
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch != 'e' && ch != 'q')
+{
 putchar(ch);
-for (ch = 'A'; ch <= 'Z'; ch++)
-putchar(ch);
+}
+ch++;
+}
 putchar('\n');
 return (0);
 }
